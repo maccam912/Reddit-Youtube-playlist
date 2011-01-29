4a22e4b3272b23e9441434062f03bd3876e84757
Matt <maccam912@gmail.com>
2011-01-29 14:35:53 -0600

    nokogiri instead of hpricot

--- app/controllers/youtubeplaylist_controller.rb
+++ app/controllers/youtubeplaylist_controller.rb
@@ -12,14 +12,14 @@ class YoutubeplaylistController < ApplicationController
     $count = -1
     while ($pagestart <= $pageend) do
       $site = open($link)
-      $doc = Hpricot($site)
-      $doc.search("a[$href]").each do |href|
+      $doc = Nokogiri::HTML($site)
+      $doc.search("a[href]").each do |href|
         $links << href.to_s
       end
       if $pagestart == 1
-        $link = $doc.at("//html/body/div[4]/p/a")['href']
+        $link = $doc.xpath("//html/body/div[4]/p/a").to_s[9..-31]
       elsif $pagestart != 1
-        $link = $doc.at("//html/body/div[4]/p/a[2]")['href']
+        $link = $doc.xpath("//html/body/div[4]/p/a[2]").to_s[9..-31]
       end
       puts $link
       $pagestart += 1
@@ -36,21 +36,22 @@ class YoutubeplaylistController < ApplicationController
     end
     $youtubecodes.uniq!
     $durations = Array.new
-    $durations << 3
+    $durations << "3"
     $titles = Array.new
     $titles << "Loading..."
     $youtubecodes.each do |ytc|
       puts ytc
       begin
-        video = Hpricot.XML(open("http://gdata.youtube.com/feeds/api/videos/#{ytc}"))
+        video = Nokogiri::XML(open("http://gdata.youtube.com/feeds/api/videos/#{ytc}"))
       rescue
         puts "Error while opening id #{ytc}"
         $youtubecodes.delete("#{ytc}")
         next
       end
-      $durations << video.at("yt:duration").attributes['seconds']
-      $titles <<  video.search("title").inner_html
+      $durations << video.xpath("//yt:duration").to_s[22..24].to_i
+      $titles <<  video.search("title").text
     end
+    puts $durations
     $number = $youtubecodes.length
   end
   def youtubeframe
