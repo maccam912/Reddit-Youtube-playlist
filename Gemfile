9d105dcb326f0bbb6bdb0500bbd543205da1885a
Matt <maccam912@gmail.com>
2011-01-29 00:19:56 -0600

    working

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'hpricot'
-gem 'video_info'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/youtubeplaylist_controller.rb
+++ app/controllers/youtubeplaylist_controller.rb
@@ -4,40 +4,40 @@ class YoutubeplaylistController < ApplicationController
   def index
   end
   def playlist
-    @links = Array.new
-    @link = params['link']
+    $links = Array.new
+    $link = params['link']
     $count = -1
-    @site = open(@link)
-    @doc = Hpricot(@site)
-    @doc.search("a[@href]").each do |href|
-      @links << href.to_s
+    $site = open($link)
+    $doc = Hpricot($site)
+    $doc.search("a[$href]").each do |href|
+      $links << href.to_s
     end
-    @youtubes = Array.new
-    @links.each do |link|
+    $youtubes = Array.new
+    $links.each do |link|
       if link.match("http:\\/\\/www\\.youtube\\.com\\/watch\\?v=")
-        @youtubes << link
+        $youtubes << link
       end
     end
-    @youtubecodes = Array.new
-    @youtubes.each do |yt|
-      @youtubecodes << yt[55..65]
+    $youtubecodes = Array.new
+    $youtubes.each do |yt|
+      $youtubecodes << yt[55..65]
     end
-    @durations = Array.new
-    @durations << 5
-    @youtubecodes.each do |ytc|
-      video = VideoInfo.new("http://www.youtube.com/watch?v=#{ytc}")
-      @durations << (video.duration + 3)
+    $durations = Array.new
+    $durations << 5
+    $youtubecodes.each do |ytc|
+      video = Hpricot.XML(open("http://gdata.youtube.com/feeds/api/videos/#{ytc}"))
+      $durations << video.at("yt:duration").attributes['seconds']
     end
   end
   def youtubeframe
     $count += 1
-    @time = Array.new
-    @durations.each do |d|
-      @time << d
+    $time = Array.new
+    $durations.each do |d|
+      $time << d
     end
-    @youtubeurl = ["http://www.whatismyip.org"]
-    @youtubecodes.each do |ytc|
-      @youtubeurl << "http://www.youtube.com/v/#{ytc}"
+    $youtubeurl = ["http://www.whatismyip.org"]
+    $youtubecodes.each do |ytc|
+      $youtubeurl << "http://www.youtube.com/v/#{ytc}"
     end
   end
 end
